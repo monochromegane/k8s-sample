8d154cf60b8a0c2a1400421d0a918841295020af
monochromegane <dev.kuro.obi@gmail.com>
2015-09-06 10:21:34 +0900

    Added k8s service account token.

--- itamae/cookbooks/kubernetes/master/default.rb
+++ itamae/cookbooks/kubernetes/master/default.rb
@@ -1,4 +1,11 @@
+# see: https://github.com/kubernetes/kubernetes/issues/11355#issuecomment-127378691
+execute 'generate service account token' do
+  command 'mkdir -p /etc/pki/kube-apiserver/ && openssl genrsa -out /etc/pki/kube-apiserver/serviceaccount.key 2048'
+  not_if 'test -e /etc/pki/kube-apiserver/serviceaccount.key'
+end
+
 template '/etc/kubernetes/apiserver'
+template '/etc/kubernetes/controller-manager'
 
 service 'kube-apiserver' do
   action [:start, :enable]
